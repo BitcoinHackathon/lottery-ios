10b6579c26bc89bd6731f60a1354c81d45bf7023
tommy19970714 <toshiki0714@icloud.com>
2018-09-22 17:01:45 +0900

    create project

---
+++ lottery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lottery
+//
+//  Created by 冨平準喜 on 2018/09/22.
+//  Copyright © 2018年 冨平準喜. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
