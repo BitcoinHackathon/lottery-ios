2e10815d704bca07516e8c2551a4fcb2157e3534
tommy19970714 <toshiki0714@icloud.com>
2018-09-22 22:42:04 +0900

    add detail view controller

---
+++ lottery/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  lottery
+//
+//  Created by 冨平準喜 on 2018/09/22.
+//  Copyright © 2018年 冨平準喜. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, StoryboardInstantiatable {
+    
+    static var nibOptions: [UINib.OptionsKey : Any]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
